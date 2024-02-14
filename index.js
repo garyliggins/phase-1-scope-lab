611c26efbe9f01160172d41ad0ee1a2c74475f1e
gary <garyliggins@yahoo.com>
2024-02-13 21:12:40 -0800

    complete

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomerName() {
+    
+        customerName = customerName.toUpperCase();
+    
+}
+
+// function upperCaseCustomerName() {
+//    customerName.toUpperCase(); 
+   
+        
+// }
+// upperCaseCustomerName();
+
+
+function setBestCustomer(){
+
+    bestCustomer = 'not bob';
+    
+}
+
+function overwriteBestCustomer(){
+
+    bestCustomer = 'maybe bob';
+
+}
+
+const leastFavoriteCustomer = "Gary"
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = "john"
+}
